d8f419a282a5cf8aca85b5e321af28a309b03341
Michael <hovgaard1990@gmail.com>
2022-11-30 10:32:42 +0100

    BTM nav

---
+++ my-app/src/components/btm-nav/btmNav.jsx
@@ -0,0 +1,14 @@
+import './btmNav.css'
+
+export default function btmNav() {
+  return (
+    <nav className='navBtm'>
+
+  <svg width="100vw" height="auto" viewBox="0 0 428 108" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M0 81.8908C165 69.7555 135.5 -0.000136566 216 1.64175e-10C296.5 0.000136567 274 81.8908 428 81.8908V108H0V81.8908Z" fill="#34430F"/>
+</svg>
+
+    </nav>
+
+  )
+}
