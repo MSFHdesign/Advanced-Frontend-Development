b7fa8e115a81b27b39d56f4367216096a373413d
MagnusKingoDanielsen <magnus@metteogivar.dk>
2022-11-30 10:35:05 +0100

    b

--- my-app/src/pages/start-intro/start-intro.jsx
+++ my-app/src/pages/start-intro/start-intro.jsx
@@ -1,10 +1,12 @@
 import "../../pages/start-intro/start-intro.css";
 import logo from "../../pics/logo.svg";
+import btmpart from "../../pics/bottompart.svg";
 
 export default function StartIntro() {
   return (
     <div className="test">
       <img className="logo" src={logo} alt="logo"></img>
+      <img className="logo" src={btmpart} alt="logo"></img>
       <h1>Kirkegårds historier</h1>
       <div className="buttons">
         <button>Start</button>
