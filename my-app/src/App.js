4250f5d7d16987a6f23e3a7aa34af9d195f92026
Michael <hovgaard1990@gmail.com>
2022-11-30 09:09:21 +0100

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,11 +2,14 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom'; 
  
 
-import './App.css';
+import './app.css';
 
 function App() {
   return (
+
+
     <div className="App">
+
       <Routes> 
          <Route/>
       </Routes>
