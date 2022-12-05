23cd280c7d23e14699798a86ea74ff185bf34752
Michael <hovgaard1990@gmail.com>
2022-12-05 13:44:56 +0100

    QR
    Husk NPM i

--- my-app/src/components/btm-nav/btmNav.jsx
+++ my-app/src/components/btm-nav/btmNav.jsx
@@ -2,28 +2,25 @@ import "./btmNav.css";
 import HomeGreen from "../../pics/homegreen.svg";
 import MapGreen from "../../pics/mapgreen.svg";
 import QRscanner from "../../pics/qrscansolidgreen.svg";
+import { Link } from "react-router-dom";
 
 export default function btmNav() {
   return (
     <nav className="navBtm">
-      <div className="iconFlex">
-        <img src={HomeGreen} alt="Home" className="HomePic" />
-        <img src={QRscanner} alt="HQR" className="QRscanner" />
-        <img src={MapGreen} alt="Map" className="Pointer" />
-      </div>
+   
+      <Link to="/">
+        <img src={HomeGreen} alt="Home" className="Icon" /> 
+      </Link>
+      <Link to="/Qr">
+        <img src={QRscanner} alt="HQR" className="Icon" />
+      </Link>
+      <Link to="/">  
+        <img src={MapGreen} alt="Map" className="Icon" />
+      </Link>  
+      <Link to="/">
+          <img src={MapGreen} alt="AddStory" className="Icon" />
+      </Link>
 
-      <svg
-        width="100vw"
-        height="100%"
-        viewBox="0 0 428 108"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <path
-          d="M0 81.8908C165 69.7555 135.5 -0.000136566 216 1.64175e-10C296.5 0.000136567 274 81.8908 428 81.8908V108H0V81.8908Z"
-          fill="#34430F"
-        />
-      </svg>
     </nav>
   );
 }
