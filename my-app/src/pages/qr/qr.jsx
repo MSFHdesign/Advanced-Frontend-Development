f3a86fc460850f3a741e2e6d100666a9038308c2
MagnusKingoDanielsen <magnus@metteogivar.dk>
2022-12-08 12:17:56 +0100

    h

--- my-app/src/pages/qr/qr.jsx
+++ my-app/src/pages/qr/qr.jsx
@@ -1,41 +1,41 @@
-import React, { Component } from 'react'
-import QrReader from 'react-qr-scanner'
+import "../../pages/qr/qr.css";
+import React, { useState } from "react";
+import QrReader from "react-qr-scanner";
 
-class Qr extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      delay: 100,
-      result: 'No result',
+const Example = () => {
+  const delay = 500;
+
+  const previewStyle = {
+    height: "auto",
+    width: "100%",
+  };
+
+  const [result, setResult] = useState(
+    "Scan QR koden på gravsteder for at finde historien"
+  );
+
+  const handleScan = (result) => {
+    if (result) {
+      setResult(result);
     }
+  };
 
-    this.handleScan = this.handleScan.bind(this)
-  }
-  handleScan(data){
-    this.setState({
-      result: data,
-    })
-  }
-  handleError(err){
-    console.error(err)
-  }
-  render(){
-    const previewStyle = {
-      height: 240,
-      width: 320,
-    }
+  const handleError = (error) => {
+    console.log(error);
+  };
 
-    return(
-      <div>
-        <QrReader
-          delay={this.state.delay}
-          style={previewStyle}
-          onError={this.handleError}
-          onScan={this.handleScan} 
-          />
-        <p>{this.state.result}</p>
-      </div>
-    )
-  }
-}
-export default Qr
+  return (
+    <>
+      <QrReader
+        className="QRreader"
+        delay={delay}
+        style={previewStyle}
+        onError={handleError}
+        onScan={handleScan}
+      />
+      <p>{result}</p>
+    </>
+  );
+};
+
+export default Example;
