35fd1fa147de96e20fe2deaad7ee33392d928ba9
MagnusKingoDanielsen <magnus@metteogivar.dk>
2022-12-12 10:15:31 +0100

    Update map.jsx

--- my-app/src/pages/map/map.jsx
+++ my-app/src/pages/map/map.jsx
@@ -1,41 +1,40 @@
-import "../../pages/map/map.css";
-import React, { useRef, useEffect, useState } from 'react';
-import mapboxgl from '!mapbox-gl'; // eslint-disable-line import/no-webpack-loader-syntax
- 
-mapboxgl.accessToken = 'pk.eyJ1IjoibWljaGFlbHNpaW4iLCJhIjoiY2wwdXFqOTdqMHFqMDNibnA5bzZwNmticCJ9.bk64oR07ab_vT6MATp-h0w';
- 
-export default function App() {
-const mapContainer = useRef(null);
-const map = useRef(null);
-const [lng, setLng] = useState(10.156662408225571);
-const [lat, setLat] = useState(56.15600023731287);
-const [zoom, setZoom] = useState(15);
+// import "../../pages/map/map.css";
+// import React, { useRef, useEffect, useState } from "react";
+// import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
 
-useEffect(() => {
-if (map.current) return; // initialize map only once
-map.current = new mapboxgl.Map({
-container: mapContainer.current,
-style: 'mapbox://styles/mapbox/streets-v12',
-center: [lng, lat],
-zoom: zoom
-});
-});
- 
-useEffect(() => {
-if (!map.current) return; // wait for map to initialize
-map.current.on('move', () => {
-setLng(map.current.getCenter().lng.toFixed(4));
-setLat(map.current.getCenter().lat.toFixed(4));
-setZoom(map.current.getZoom().toFixed(2));
-});
-});
- 
-return (
-<div>
-<div className="sidebar">
+// mapboxgl.accessToken =
+//   "pk.eyJ1IjoibWljaGFlbHNpaW4iLCJhIjoiY2wwdXFqOTdqMHFqMDNibnA5bzZwNmticCJ9.bk64oR07ab_vT6MATp-h0w";
 
-</div>
-<div ref={mapContainer} className="map-container" />
-</div>
-);
-}
+// export default function App() {
+//   const mapContainer = useRef(null);
+//   const map = useRef(null);
+//   const [lng, setLng] = useState(10.156662408225571);
+//   const [lat, setLat] = useState(56.15600023731287);
+//   const [zoom, setZoom] = useState(15);
+
+//   useEffect(() => {
+//     if (map.current) return; // initialize map only once
+//     map.current = new mapboxgl.Map({
+//       container: mapContainer.current,
+//       style: "mapbox://styles/mapbox/streets-v12",
+//       center: [lng, lat],
+//       zoom: zoom,
+//     });
+//   });
+
+//   useEffect(() => {
+//     if (!map.current) return; // wait for map to initialize
+//     map.current.on("move", () => {
+//       setLng(map.current.getCenter().lng.toFixed(4));
+//       setLat(map.current.getCenter().lat.toFixed(4));
+//       setZoom(map.current.getZoom().toFixed(2));
+//     });
+//   });
+
+//   return (
+//     <div>
+//       <div className="sidebar"></div>
+//       <div ref={mapContainer} className="map-container" />
+//     </div>
+//   );
+// }
