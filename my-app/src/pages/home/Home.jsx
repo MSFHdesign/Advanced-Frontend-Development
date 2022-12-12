e792a950275a600234027d9f47d126e3f390ec6c
MultimedieGunvor <eaagupa@students.eaaa.dk>
2022-12-12 09:15:21 +0100

    jcrwuipfhi

--- my-app/src/pages/home/Home.jsx
+++ my-app/src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
-import TopNav from '../../components/top-nav/topnav'
+import TopNav from '../../components/top-nav/topnav';
+import StoryDisplay from '../../pages/Story/Story_Display'
 
 export default function Home() {
   return (
       <>
       <TopNav />
         <h2> Home </h2>
-
+        <StoryDisplay />
 
 
 
