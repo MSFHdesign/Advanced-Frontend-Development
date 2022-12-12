0fe8e01b155d067264842d299b65f9fec11086a5
Michael <hovgaard1990@gmail.com>
2022-12-12 10:34:18 +0100

    1

--- my-app/src/pages/home/Home.jsx
+++ my-app/src/pages/home/Home.jsx
@@ -1,12 +1,14 @@
 import TopNav from '../../components/top-nav/topnav'
 
+import ArticleDisplay from '../Story/Article_Display'
+
 export default function Home() {
   return (
       <>
       <TopNav />
         <h2> Home </h2>
 
-
+    <ArticleDisplay/>
 
 
     </>
