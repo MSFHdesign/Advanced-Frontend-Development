347cc93539656afe34a95b9a6b869e8b5ea1030d
Michael <hovgaard1990@gmail.com>
2022-12-13 08:18:15 +0100

    Update Home.jsx

--- my-app/src/pages/home/Home.jsx
+++ my-app/src/pages/home/Home.jsx
@@ -1,7 +1,5 @@
 import TopNav from '../../components/top-nav/topnav';
-import StoryDisplay from '../../pages/Story/Story_Display'
-
-import ArticleDisplay from '../Story/Article_Display'
+import StoryDisplay from '../../pages/Story/Story_Display';
 
 export default function Home() {
   return (
