944395bfcd36bf2b16084f80f2e3a739f6f37e0f
MagnusKingoDanielsen <magnus@metteogivar.dk>
2022-12-13 12:50:24 +0100

    d

--- my-app/src/pages/home/Home.jsx
+++ my-app/src/pages/home/Home.jsx
@@ -1,14 +1,14 @@
-import TopNav from '../../components/top-nav/topnav';
-import StoryDisplay from '../../pages/Story/Story_Display'
+import TopNav from "../../components/top-nav/topnav";
+import StoryDisplay from "../../pages/Story/Story_Display";
 // eslint-disable-next-line no-unused-vars
-import ArticleDisplay from '../Story/Article_Display'
+import ArticleDisplay from "../Story/Article_Display";
 
 export default function Home() {
   return (
-      <>
+    <>
       <TopNav />
-        {/* <h2> Home </h2> */}
-        <StoryDisplay />
+      {/* <h2> Home </h2> */}
+      <StoryDisplay />
     </>
-  )
+  );
 }
