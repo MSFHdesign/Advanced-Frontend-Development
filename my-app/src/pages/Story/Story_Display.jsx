eae52c7e6fa9a12686f3f84259494c4c7d175a6e
Michael <hovgaard1990@gmail.com>
2022-12-13 11:30:20 +0100

    Update Story_Display.jsx

--- my-app/src/pages/Story/Story_Display.jsx
+++ my-app/src/pages/Story/Story_Display.jsx
@@ -15,10 +15,10 @@ export default function Articles() {
     SetArticles(
       Articles.filter((Articles) =>
         //Filters
-        Articles.name.toLowerCase().includes(search.toLowerCase()),
-        Articles.dead.toString().includes(search.toString()),
-        Articles.born.toString().includes(search.toString()),
-        Articles.graveId.toString().includes(search.toString()),
+        Articles.name.toLowerCase().includes(search.toLowerCase())||
+        Articles.dead.toString().includes(search.toString())||
+        Articles.born.toString().includes(search.toString())||
+        Articles.graveId.toString().includes(search.toString())
         )
     );
   };
