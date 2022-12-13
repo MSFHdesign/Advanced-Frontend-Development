a1f190047116a03a49fc42259ce17b4e032c48ef
MagnusKingoDanielsen <magnus@metteogivar.dk>
2022-12-13 13:35:41 +0100

    i

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,7 +8,7 @@ import { Route, Routes } from "react-router-dom";
 
 import StartIntro from "./pages/start-intro/start-intro";
 import Qr from "./pages/qr/qr";
-// import Map from "./pages/map/map";
+import Map from "./pages/map/map";
 import Story from "./pages/Story/Story.jsx";
 import Home from "./pages/home/Home.jsx";
 import Topnav from "./components/top-nav/topnav";
@@ -22,8 +22,7 @@ import Livshistorie from "./pages/Story/OpenStory.jsx";
 
 import NameStory from "./pages/Story/OpenArticle";
 
-import NameTest from "./pages/Story/Article_Display"
-
+import NameTest from "./pages/Story/Article_Display";
 
 function App() {
   return (
@@ -32,14 +31,12 @@ function App() {
         <Route path="/" element={<StartIntro />} />
         <Route path="/home" element={<Home />} />
         <Route path="/*" element={<Home />} />
-        {/* <Route path="/Map" element={<Map />} /> */}
+        <Route path="/Map" element={<Map />} />
         <Route path="/Qr" element={<Qr />} />
-    
-        <Route path="/qr/:Navn" element={<NameStory />} />
-    
-        
-        <Route path="/Qr/TESTER" element={<NameTest />} />
 
+        <Route path="/qr/:Navn" element={<NameStory />} />
+
+        <Route path="/Qr/TESTER" element={<NameTest />} />
 
         <Route path="/story" element={<Story />} />
         <Route path="/step0" element={<Step0 />} />
