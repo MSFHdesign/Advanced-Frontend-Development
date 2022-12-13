2bd6e34d1168d1a957f5675cffef29eccfff7100
MultimedieGunvor <eaagupa@students.eaaa.dk>
2022-12-13 14:16:12 +0100

    vgjk

--- my-app/src/pages/Story/Story_add.jsx
+++ my-app/src/pages/Story/Story_add.jsx
@@ -4,12 +4,8 @@ import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { storage, db } from "../../firebaseConfig";
 import { toast } from "react-toastify";
 import AddedModal from "./AddedModal";
-// import { NavLink } from "react-router-dom";
-
-
-
-
 import JobIcons from "./ChooseJobIcon";
+
 
 export default function AddArticle() {
   // form clear after submit
@@ -41,7 +37,7 @@ export default function AddArticle() {
     setFormData({ ...formData, image: e.target.files[0] });
   };
 
-  // workstuff ckeckboxes
+  // workstuff checkboxes
   const [isShown, setIsShown] = useState(false);
   const showWorkIcons = event => {
     setIsShown(current => !current);
@@ -291,15 +287,12 @@ export default function AddArticle() {
       </div>
 
       <div className="submitHistoryBtn">
-        {/* <button className="btn formsubmit" onClick={handlePublish}>
-          <svg width="30" height="30" viewBox="0 0 392 392" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M196,0C87.75,0,0,87.75,0,196s87.75,196,196,196,196-87.75,196-196S304.25,0,196,0Zm119.25,83.23c-97.96,95.74-144.92,239.42-145.39,240.86-1.93,6-7.41,10.16-13.7,10.4-.19,0-.39,.01-.58,.01-6.07,0-11.57-3.67-13.88-9.32L73.35,158.04c-3.13-7.67,.54-16.43,8.21-19.56,7.67-3.13,16.43,.54,19.56,8.21l53.87,131.75c5.88-14.3,13.69-32.03,23.53-51.77,22.92-46.01,61.61-111.97,115.76-164.9,5.92-5.79,15.42-5.68,21.21,.24s5.68,15.42-.24,21.21Z" fill="#8b9769"/>
-          </svg> 
-          Færdig
-        </button> */}
 
         <button className="btn formsubmit" onClick={openModal}>
-          Vis livshistorie
+        <svg width="30" height="30" viewBox="0 0 392 392" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M196,0C87.75,0,0,87.75,0,196s87.75,196,196,196,196-87.75,196-196S304.25,0,196,0Zm119.25,83.23c-97.96,95.74-144.92,239.42-145.39,240.86-1.93,6-7.41,10.16-13.7,10.4-.19,0-.39,.01-.58,.01-6.07,0-11.57-3.67-13.88-9.32L73.35,158.04c-3.13-7.67,.54-16.43,8.21-19.56,7.67-3.13,16.43,.54,19.56,8.21l53.87,131.75c5.88-14.3,13.69-32.03,23.53-51.77,22.92-46.01,61.61-111.97,115.76-164.9,5.92-5.79,15.42-5.68,21.21,.24s5.68,15.42-.24,21.21Z" fill="#8b9769"/>
+        </svg>
+          Færdig
         </button>
         <AddedModal title="Post livshistorie?" onClose={closeModal} show={show}>
           <button className="btn formsubmit" onClick={() => {
@@ -314,7 +307,6 @@ export default function AddArticle() {
         <button onClick={closeModal}>
           Tilbage
         </button>
-
         </AddedModal>
       </div>
     </div>
