26cf8e9fa3468b6b4737f8456a8ff7cda31959ee
MultimedieGunvor <eaagupa@students.eaaa.dk>
2022-12-14 10:09:26 +0100

    Added icons

--- my-app/src/components/top-nav/topnav.jsx
+++ my-app/src/components/top-nav/topnav.jsx
@@ -1,12 +1,16 @@
 import './topnavstyle.css'
 import logo from '../../pics/logo.svg'
 import { Link } from "react-router-dom";
+import info from '../../pics/icons/Info1.svg'
 
 function Topnav() {
   return (
     <div className='topNavbox'>
       <Link to= "/" >
         <img src={logo} alt="Kirkegårds appen" className='srcLogo'/>
+      </Link>
+      <Link to= "/step0" >
+        <img src={info} alt="Info-logo" className='srcLogo'/>
       </Link>
     </div>
   )
