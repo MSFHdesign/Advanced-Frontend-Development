5a5960e0fd355704cfcc1d532ee4baa000570a64
Skydstofte <rikke-sc@hotmail.com>
2022-12-14 10:59:17 +0100

    fghjkl

--- my-app/src/pages/Story/Story_add.jsx
+++ my-app/src/pages/Story/Story_add.jsx
@@ -4,9 +4,20 @@ import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { storage, db } from "../../firebaseConfig";
 import { toast } from "react-toastify";
 import AddedModal from "./AddedModal";
-import JobIcons from "./ChooseJobIcon";
 import Topnav from "../../components/top-nav/topnav";
 
+// job icons
+import book from "../../pics/jobicons/book.svg";
+import briefcase from "../../pics/jobicons/briefcase.svg";
+import dollar from "../../pics/jobicons/dollar.svg";
+import hammer from "../../pics/jobicons/hammer.svg";
+import leaf from "../../pics/jobicons/leaf.svg";
+import ruler from "../../pics/jobicons/ruler.svg";
+import saw from "../../pics/jobicons/saw.svg";
+import suit from "../../pics/jobicons/suit.svg";
+import node from "../../pics/jobicons/node.svg";
+import pencil from "../../pics/jobicons/pencil.svg";
+import noIcon from "../../pics/jobicons/NoIcon.svg";
 
 export default function AddArticle() {
   // form clear after submit
@@ -262,7 +273,23 @@ export default function AddArticle() {
         </div>
 
         {isShown && (
-          <JobIcons/>
+          <div className="jobIconContainer">
+          <h4>Vælg ikoner som passer til erhvervet</h4>
+          <input type="text" placeholder="Indtast erhverv..." id="workName" />
+          <div className="jobIcons">
+              <li><input type="checkbox" name="jobIcon" id="jobIconbox1"/><label htmlFor="jobIconbox1"><img src={book} alt="Bog"/></label></li>
+              <li><input type="checkbox" name="jobIcon" id="jobIconbox2"/><label htmlFor="jobIconbox2"><img src={briefcase} alt="Taske"/></label></li>
+              <li><input type="checkbox" name="jobIcon" id="jobIconbox3"/><label htmlFor="jobIconbox3"><img src={dollar} alt="Penge"/></label></li>
+              <li><input type="checkbox" name="jobIcon" id="jobIconbox4"/><label htmlFor="jobIconbox4"><img src={hammer} alt="Hammer"/></label></li>
+              <li><input type="checkbox" name="jobIcon" id="jobIconbox5"/><label htmlFor="jobIconbox5"><img src={leaf} alt="Blade"/></label></li>
+              <li><input type="checkbox" name="jobIcon" id="jobIconbox6"/><label htmlFor="jobIconbox6"><img src={ruler} alt="lineal"/></label></li>
+              <li><input type="checkbox" name="jobIcon" id="jobIconbox7"/><label htmlFor="jobIconbox7"><img src={saw} alt="Sav"/></label></li>
+              <li><input type="checkbox" name="jobIcon" id="jobIconbox8"/><label htmlFor="jobIconbox8"><img src={suit} alt="Jakkesæt"/></label></li>
+              <li><input type="checkbox" name="jobIcon" id="jobIconbox9"/><label htmlFor="jobIconbox9"><img src={node} alt="Node"/></label></li>
+              <li><input type="checkbox" name="jobIcon" id="jobIconbox10"/><label htmlFor="jobIconbox10"><img src={pencil} alt="Blyant"/></label></li>
+              <li><input type="checkbox" name="jobIcon" id="jobIconbox11"/><label htmlFor="jobIconbox11"><img src={noIcon} alt="Intet valg"/></label></li>
+          </div>
+      </div>
         )}
 
         <div className="historyInput">
