1f8219769c751a64130ad25b37f964088785244c
MagnusKingoDanielsen <magnus@metteogivar.dk>
2022-12-14 12:16:58 +0100

    a

--- my-app/src/pages/Story/OpenArticle.jsx
+++ my-app/src/pages/Story/OpenArticle.jsx
@@ -3,12 +3,12 @@ import { collection, onSnapshot, orderBy, query } from "firebase/firestore";
 import { db } from "../../firebaseConfig";
 import stjerne from "../../pics/shapes/stjerne.svg";
 import kors from "../../pics/shapes/kors.svg";
-import Ingenioer from "../../pics/jobicons/ruler.svg";
+import "../../pages/Story/OpenArticle.css";
 
 export default function HistoryPrivate() {
   const [Artikler, SetArtikler] = useState([]);
   const filter = sessionStorage.QrNavn;
- 
+
   useEffect(() => {
     const articleRef = collection(db, "Artikler");
     const q = query(articleRef, orderBy("Navn"));
@@ -29,16 +29,31 @@ export default function HistoryPrivate() {
         <p> Nothing to see, yet....</p>
       ) : (
         Artikler.map(
-          ({ Navn, id, born, dead, resume, lifestory, job, url, GraveId }) =>
+          ({
+            Navn,
+            id,
+            born,
+            dead,
+            resume,
+            lifestory,
+            job,
+            url,
+            Icon,
+            GraveId,
+          }) =>
             id === filter ? (
-              <div className="StoriesBox" key={id}>
-                <div className="StoryBox">
+              <div className="StoriesBox" id="StoriesBox" key={id}>
+                <div className="StoryBox" id="StoryBox">
                   <h2>{Navn}</h2>
-                  <img src={stjerne} alt="Stjerne" /> {born}{" "}
-                  <img src={kors} alt="Kors" /> {dead}
-                  <img src={url}/> <br/>
-                  <div className={job}/>
-                  <img src={{job}} alt={job} width='100px' height='100px'/>
+                  <h3>
+                    <img src={stjerne} alt="Stjerne" /> {born}{" "}
+                    <img src={kors} alt="Kors" /> {dead}
+                  </h3>
+                  <div id="StoryBoxImg">
+                    <img src={url} />
+                  </div>
+                  <div className={Icon} />
+                  <p id="job">{job}</p>
                   <h1>Resume</h1>
                   <p>{resume}</p>
                   <h1>Livs historie</h1>
