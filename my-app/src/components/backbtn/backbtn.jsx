a38dfe5805802f83609670458ca16abe9870be24
Skydstofte <rikke-sc@hotmail.com>
2022-12-15 09:26:21 +0100

    lkjhg

--- my-app/src/components/backbtn/backbtn.jsx
+++ my-app/src/components/backbtn/backbtn.jsx
@@ -1,11 +1,15 @@
+import './backbtn.css'
 import React from 'react'
 import { useNavigate } from 'react-router-dom';
+import back from '../../pics/navicon/Pil.svg'
 
 function Backbtn() {
     const navigate = useNavigate();
   return (
     <div>
-        <button className="backBTN" onClick={() => navigate(-1)}>Go back</button>
+        <button className="backBTN" onClick={() => navigate(-1)}>
+          <img src={back} alt="Info-logo"/>
+        </button>
     </div>
   )
 }
